6aaae04be6a83c480a5758524be74f3ce00403ad
Sara Helal <s.helal91@gmail.com>
2025-08-17 04:15:34 +0300

    Search page test done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Layout from "@/components/layouts/Layout";
+import SearchPage from "@/components/ui/SearchPage";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <Layout>
-     <h1></h1>
-   </Layout>
+  //  <Layout>
+  //    <h1></h1>
+  //  </Layout>
+  <SearchPage />
   );
 }
